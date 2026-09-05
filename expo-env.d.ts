42ad2e79a1c8cef36fb68a2c93addaa001e1158b
sanchitamodi <modi.sanchita@gmail.com>
2026-09-05 15:08:22 -0700

    correct supabase url

--- expo-env.d.ts
+++ expo-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="expo/types" />
 
-// NOTE: This file should not be edited and should be in your Git ignore.
+// NOTE: This file should not be edited and should be in your git ignore
